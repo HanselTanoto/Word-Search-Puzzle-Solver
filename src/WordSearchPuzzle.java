6bd28457df35a00b8eeb5623f9d735240ae6c399
HanselTanoto <hansel.tanoto05@gmail.com>
2022-01-21 21:17:21 +0700

    horizontal and vertical string matching

--- src/WordSearchPuzzle.java
+++ src/WordSearchPuzzle.java
@@ -23,15 +23,27 @@ public class WordSearchPuzzle {
 
         char[][] puzzle = new char[row][col];
         String[] words = new String[wCount];
+        int[][] solved = new int[row][col];
+
         readFile(path, puzzle, words);
         writePuzzle(puzzle);
         writeWords(words);
-
+        
         /*
         System.out.println(row);
         System.out.println(col);
         System.out.println(wCount);
         */
+
+        for (int i = 0; i < wCount; i++) {
+            int colorId = i%11 + 1;
+            down(puzzle,words[i],solved,colorId);
+            up(puzzle,words[i],solved,colorId);
+            right(puzzle,words[i],solved,colorId);
+            left(puzzle,words[i],solved,colorId);
+        }
+        writeSolved(puzzle, solved);
+ 
         
         sc.close();
         return;
@@ -61,6 +73,15 @@ public class WordSearchPuzzle {
         return result;
     }
     
+
+    // COLOR
+    public static final String ANSI_RESET   = "\u001B[0m";
+    public static final String ITALIC		= "\u001B[3m";
+    public static final String UNDERLINE	= "\u001B[4m";
+    public static final String ANSI_COLOR[] = {"\u001B[37m", "\u001B[32m", "\u001B[33m", "\u001B[34m", "\u001B[35m", "\u001B[36m", "\u001B[91m", "\u001B[92m", "\u001B[93m", "\u001B[94m", "\u001B[95m", "\u001B[96m"};
+
+
+    // READ AND WRITE
     public static void readFile(String path, char[][] puzzle, String[] words) throws Exception {
         String line;
         Scanner sc = new Scanner(new BufferedReader(new FileReader(path)));
@@ -95,6 +116,107 @@ public class WordSearchPuzzle {
         for (int i = 0; i < words.length; i++) {
             //System.out.println(puzzle[i][j]);
             System.out.println(words[i]);
+        }
+    }
+    
+    public static void writeSolved (char[][] puzzle, int[][] solved) {
+        System.out.println("\nSolved:");
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[0].length; j++) {
+                //System.out.println(puzzle[i][j]);
+                //System.out.printf(ANSI_COLOR[solved[i][j]]);
+                System.out.printf(ANSI_COLOR[solved[i][j]]);
+                if (solved[i][j] != 0) {
+                    System.out.printf(ITALIC);
+                    System.out.printf(UNDERLINE);
+                }
+                System.out.printf("%c ", puzzle[i][j]);
+                System.out.printf(ANSI_RESET);
+            }
+            System.out.printf("\n");
+        }
+    }
+
+
+    // PROSEDUR STRING MATCHING
+    public static void down (char[][] puzzle, String word, int[][] solved, int colorId) {
+        int row = puzzle.length;
+        int col = puzzle[0].length;
+        int wordLen = word.length();
+        for (int h = 0; h < col; h++) {
+            for (int i = 0; i < row-wordLen; i++) {
+                int j = 0;
+                while ((j < wordLen) && (puzzle[i+j][h] == word.charAt(j))) {
+                    j++;
+                }
+                if (j == wordLen) {
+                    for (int k = i; k < i+wordLen; k++) {
+                        solved[k][h] = colorId;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void up (char[][] puzzle, String word, int[][] solved, int colorId) {
+        int row = puzzle.length;
+        int col = puzzle[0].length;
+        int wordLen = word.length();
+        for (int h = 0; h < col; h++) {
+            for (int i = row-1; i >= wordLen; i--) {
+                int j = 0;
+                while ((j < wordLen) && (puzzle[i-j][h] == word.charAt(j))) {
+                    j++;
+                }
+                if (j == wordLen) {
+                    for (int k = i; k > i-wordLen; k--) {
+                        solved[k][h] = colorId;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void right (char[][] puzzle, String word, int[][] solved, int colorId) {
+        int row = puzzle.length;
+        int col = puzzle[0].length;
+        int wordLen = word.length();
+        for (int h = 0; h < row; h++) {
+            for (int i = 0; i < col-wordLen; i++) {
+                int j = 0;
+                while ((j < wordLen) && (puzzle[h][i+j] == word.charAt(j))) {
+                    j++;
+                }
+                if (j == wordLen) {
+                    for (int k = i; k < i+wordLen; k++) {
+                        solved[h][k] = colorId;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void left (char[][] puzzle, String word, int[][] solved, int colorId) {
+        int row = puzzle.length;
+        int col = puzzle[0].length;
+        int wordLen = word.length();
+        
+        for (int h = 0; h < row; h++) {
+            for (int i = col-1; i >= wordLen; i--) {
+                int j = 0;
+                while ((j < wordLen) && (puzzle[h][i-j] == word.charAt(j))) {
+                    j++;
+                }
+                if (j == wordLen) {
+                    for (int k = i; k > i-wordLen; k--) {
+                        solved[h][k] = colorId;
+                    }
+                    return;
+                }
+            }
         }
     }
 
